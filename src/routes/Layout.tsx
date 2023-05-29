c00da1498ac6c61e86458d0fa5e78d127d5b3b4f
Elena Volpato <talkto@elenavolpato.me>
2023-05-29 17:34:03 +0200

    WIP(Layout): fixed some texts

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -76,20 +76,23 @@ function Layout({ food }: { food: FoodList }) {
     setOpen(false)
   };
   //dialog box that opens with each element clicked
-  function SimpleDialog() {   
+  function SimpleDialog() {
     return (
       <Dialog open={open}
         onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
+        aria-labelledby="about"
+        aria-describedby="about the app"
       >
-        <DialogTitle id="alert-dialog-title">
-          {"About"}
+        <DialogTitle id="drawer-dialog-title">
+          {"About the app"}
         </DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending anonymous
-            location data to Google, even when no apps are running.
+            This project came from a personal doubt: I had recently moved to Italy and didn't know when I could find fruits and vegetables I like in the supermarket, when each food was in season. After looking for sources of it, and finding very hard to read tables, I decided to build my own app to do it.
+
+            The idea was to create it as a scalable project, so it is simple to change the database to another country, or region.
+
+            It is also an objective of this app to encourage local the consumption of local produce - that was also a thing I noticed here in Italy: here many products publicize the region they are from, I believe that it is caused by the strong regionalism that exists in Italy. There are many products that read: "cheese from Piedmont", "oranges from Sicily" or "Olive oil from Liguria". That really motivated and boosted me look for more information about local produce and to consume fruits and vegetables from local producers.
           </DialogContentText>
         </DialogContent>
         <DialogActions>
